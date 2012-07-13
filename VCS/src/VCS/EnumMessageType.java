cd5bb210ddbb07350915e6da539d756103b0e695
Carlos Ledezma <cgledezma1101@gmail.com>
2012-07-13 10:26:38 -0430

    idem

---
+++ VCS/src/VCS/EnumMessageType.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VCS;
+
+/**
+ *
+ * @author Alberto
+ */
+public enum EnumMessageType {
+    COMMIT, ENTRY, FILE_R, FILE_S
+}
