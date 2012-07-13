7fbba06fb1a602dddcfe63ad6250afd95d98339d
Carlos Ledezma <cgledezma1101@gmail.com>
2012-07-13 11:54:07 -0430

    Agregado el hilo para manejar las elecciones de servidores

---
+++ VCS/src/VCS/ServerElection.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VCS;
+
+import java.net.MulticastSocket;
+
+/**
+ *
+ * @author Guille
+ */
+public class ServerElection extends Thread{
+  private MulticastSocket listener;
+  
+  @Override
+  public void run(){
+    
+  }
+}
