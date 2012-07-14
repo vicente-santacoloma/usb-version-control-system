c7ab1aeb6a8fa4507246b0893c1edc881e055b1a
Luis Alejandro Vieira <lavz24@gmail.com>
2012-07-14 19:19:51 -0430

    New parser for XML, VersionControlImpl incomplete

---
+++ VCS/src/VCS/FileParser.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VCS;
+
+import org.dom4j.Document;
+
+/**
+ *
+ * @author lavz24
+ */
+public  class FileParser {
+  
+  
+  public static Document parserFile(String file)
+  {
+    return null;
+  }
+}
