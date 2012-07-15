09cf66ac285944519bb4fa93700f7320efcc37fe
Carlos Ledezma <cgledezma1101@gmail.com>
2012-07-15 11:27:39 -0430

    Desarrollada la clase ServerCommunication

--- VCS/src/VCS/ServerCommunication.java
+++ VCS/src/VCS/ServerCommunication.java
@@ -41,29 +41,37 @@ public class ServerCommunication extends Thread{
     Socket coord;
     FileDescription fSend;
     FileInputStream fis;
-    File in;
+    FileOutputStream fout;
+    File f;
+    Document conf;
     byte[] data;
     try{
       for(;;){
         /* 
-         * Can receive messages to perform a commit, to send files and net entry
+         * Can receive messages to perform a commit, to send files
          */
         bRecv = new byte[65535];
         pack = new DatagramPacket(bRecv, bRecv.length);
         listener.receive(pack);
         bais = new ByteArrayInputStream(pack.getData());
         ois = new ObjectInputStream(bais);
+        System.out.println(father.getId() + ": Waiting to receive a server message");
         msg = (Message) ois.readObject();
         
+        System.out.println(father.getId() + ": Message received");
+        
         /*Load this servers configuration*/
+        System.out.print(father.getId() + ": Loading server configuration...");
         for(Element s : FileParser.serverList(FileParser.parserFile("location.xml"))){
           if(Integer.parseInt(FileParser.getValueOfServer(s, "id")) != father.getId()){
             servConf = s;
             break;
           }
         }
+        System.out.println("Done");
         
         if(msg.getType() == EnumMessageType.FILE_S){
+          System.out.println(father.getId() + ": Received a request to send the files");
           /*The coordinator is requesting to receive all files contained in
            this server*/
           coord = new Socket(father.getDns().get(father.getCoordId()), 10704);
@@ -73,9 +81,9 @@ public class ServerCommunication extends Thread{
             fName = FileParser.getValueOfFile(file, "name");
 
             /*Get the files data*/
-            in = new File(fName);
-            data = new byte[(int) in.length()];
-            fis = new FileInputStream(in);
+            f = new File(fName);
+            data = new byte[(int) f.length()];
+            fis = new FileInputStream(f);
             fis.read(data);
 
             /*Create the FileDescription to be sent*/
@@ -86,19 +94,40 @@ public class ServerCommunication extends Thread{
                       FileParser.getValueOfFile(file, "user"),
                       data
                     );
-
+            System.out.print(father.getId() + ": Sending file " + fName + "...");
             oos.writeObject(fSend);
+            System.out.println("Done");
           }
             
         }else if(msg.getType() == EnumMessageType.COMMIT){
           /*An update has been made and this server must be updated*/
+          /*First update the configuration file*/
+          System.out.println(father.getId() + ": Received a new commit");
+          bais = new ByteArrayInputStream(msg.getConfigFile());
+          ois = new ObjectInputStream(bais);
+          conf = (Document) ois.readObject();
+          
+          System.out.print(father.getId() + ": Updating configuration file...");
+          FileParser.updateXMLFile("location.xml", conf);
+          System.out.println("Done");
+          
+          /*Update the appropriate files*/
           for(FileDescription fe : msg.getCommitElements()){
-           for(Element file : FileParser.getDataElements(servConf)){
-             if(!fe.getFileName().equals(FileParser.getValueOfFile(file, "name")))
-               continue;
+            for(Element file : FileParser.getDataElements(servConf)){
+              if(!fe.getFileName().equals(FileParser.getValueOfFile(file, "name")))
+                 continue;
              
-           }
+               System.out.print(father.getId() + ": Updating file" 
+                       + FileParser.getValueOfFile(file, "name")
+                       + "...");
+               fout = new FileOutputStream(FileParser.getValueOfFile(file, "name"));
+               fout.write(fe.getData());
+               System.out.println("Done");
+            }
           }
+        }else{
+          System.out.println(father.getId() + ": Received a message through "
+                  + "server communication of invalid type: " + msg.getType());
         }
       }
     }catch(SocketException se){
