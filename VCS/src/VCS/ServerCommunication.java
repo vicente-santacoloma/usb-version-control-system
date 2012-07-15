49ce34714c0d1fe22e5c0b633f07a75c88698781
Carlos Ledezma <cgledezma1101@gmail.com>
2012-07-15 10:18:23 -0430

    Commit parcial de ServerCommunication

--- VCS/src/VCS/ServerCommunication.java
+++ VCS/src/VCS/ServerCommunication.java
@@ -4,15 +4,11 @@
  */
 package VCS;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.MulticastSocket;
-import java.net.SocketException;
+import java.io.*;
+import java.net.*;
+import java.sql.Date;
+import java.util.List;
+import org.dom4j.*;
 
 /**
  *
@@ -30,7 +26,7 @@ public class ServerCommunication extends Thread{
   private ObjectInputStream ois;
   private ByteArrayOutputStream baos;
   private ObjectOutputStream oos;
-
+  List<Element> servs;
   /**
    * Constructor to build a server communication listener
    * @param listener
@@ -43,6 +39,12 @@ public class ServerCommunication extends Thread{
   
   @Override
   public void run(){
+    String fName;
+    Socket coord;
+    FileDescription fSend;
+    FileInputStream fis;
+    File in;
+    byte[] data;
     try{
       for(;;){
         /* 
@@ -58,6 +60,33 @@ public class ServerCommunication extends Thread{
         if(msg.getType() == EnumMessageType.FILE_S){
           /*The coordinator is requesting to receive all files contained in
            this server*/
+          coord = new Socket(father.getDns().get(father.getCoordId()), 10704);
+          oos = new ObjectOutputStream(coord.getOutputStream());
+          
+          servs = FileParser.serverList(FileParser.parserFile("location.xml"));
+          for(Element s : servs){
+            if(Integer.parseInt(FileParser.getValueOfServer(s, "id")) != father.getId())
+              continue;
+            
+            for(Element file : FileParser.getDataElements(s)){
+              fName = FileParser.getValueOfFile(file, "name");
+              
+              /*Get the files data*/
+              in = new File(fName);
+              data = new byte[(int) in.length()];
+              fis = new FileInputStream(in);
+              fis.read(data);
+              
+              fSend = new FileDescription(fName, 
+                      Integer.parseInt(FileParser.getValueOfFile(file, "version")), 
+                      Date.valueOf(FileParser.getValueOfFile(file, "timestamp")),
+                      FileParser.getValueOfFile(file, "user"),
+                      data
+                      );
+            }
+            
+            break;
+          }
         }else if(msg.getType() == EnumMessageType.COMMIT){
           /*An update has been made and this server must be updated*/
         }
