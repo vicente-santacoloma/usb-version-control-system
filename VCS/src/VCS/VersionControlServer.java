11547f39e1c392fa0a1f7a778a51d6d1f8e78a9c
Carlos Ledezma <cgledezma1101@gmail.com>
2012-07-15 18:58:48 -0430

    Elecciones de coordinador al iniciar el proceso

--- VCS/src/VCS/VersionControlServer.java
+++ VCS/src/VCS/VersionControlServer.java
@@ -43,6 +43,7 @@ public class VersionControlServer{
     this.hostrmi = hostrmi;
     this.portrmi = portrmi;
     this.vci = vci;
+    this.coordId = -1;
   }
   
   //Getters
@@ -127,7 +128,16 @@ public class VersionControlServer{
     p.joinGroup(group);
     
     System.out.println("Me uni al grupo");
-        
+    
+    VersionControlServer v = new VersionControlServer(null, id, hostrmi, portrmi,
+            new VersionControlImpl(p, "location.xml", 3));
+   
+    Thread election = new ServerElection(s, v);
+    election.start();
+    
+    while(v.coordId == -1)
+      Thread.sleep(6000);
+    
     VersionControl vci = (VersionControl) Naming.lookup("rmi://" + hostrmi + ":" + portrmi
             + "/VCS");
      
@@ -147,20 +157,21 @@ public class VersionControlServer{
       dns.put(Integer.parseInt(FileParser.getValueOfServer(serv, "id")),
               InetAddress.getByName(FileParser.getValueOfServer(serv, "ip"))
               );
-    
-    VersionControlServer v = new VersionControlServer(dns, id, hostrmi, portrmi,
-            new VersionControlImpl(p, dns, "location.xml", 3));
-   
-    Thread election = new ServerElection(s, v);
+
+    v.setDns(dns);
+    System.out.println("Ya tengo DNS!");
     Thread listenMessages = new ServerCommunication(p, v);
     
     listenMessages.start();
-    election.start();
     
     System.out.println("se crearon los hilos, espero por el join");
     election.join();
     listenMessages.join();
    }
+
+  public void setDns(HashMap<Integer, InetAddress> dns) {
+    this.dns = dns;
+  }
    
    public void coordFunctions(){
     System.out.println("soy coord: me conecto al rmi");
